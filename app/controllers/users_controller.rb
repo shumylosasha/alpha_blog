29f91bdda8534175495bd6d1aec639eddc1873a5
shumylosasha <shumylo.s@gmail.com>
2021-06-22 00:34:55 +0300

    Add edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
 
+  before_action :get_user, only:[:edit, :update]
+
   def new
     @user = User.new
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(form_params)
+			flash[:notice] = "User successfuly updated"
+			redirect_to articles_path
+		else
+			render "edit"
+		end
+  end
+
 
   def create
     @user = User.new(form_params)
@@ -16,6 +31,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def get_user
+    @user = User.find(params[:id])
+  end
 
   def form_params
     params.require(:user).permit(:username, :email, :password)
