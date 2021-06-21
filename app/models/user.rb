eb086a6186ea5ad43800b7adc16bbbd63cd120fd
shumylosasha <shumylo.s@gmail.com>
2021-06-21 19:05:47 +0300

    create user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
-  
+  # user must be present and unique, min3 max25
+  # email address must be present and unique, max 105
+  # email must be valid email format, check with email regex
+
+  validates :username, presence: true,
+                      length: { minimum: 3, maximum: 25 },
+                      uniqueness: { case_sensitive: false }
+  VALIDATE_EMAIL_REGEX = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { minimum: 3, maximum: 25 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALIDATE_EMAIL_REGEX}
+
+
 end
